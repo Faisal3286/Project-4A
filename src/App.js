e50b9167ddefc74f2397c491e83b31c2f39faaf0
Faisal nabeel <56247620+Faisal3286@users.noreply.github.com>
2020-07-19 22:13:25 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+import useWebAnimations from "@wellyshen/use-web-animations";
 import './App.css';
+import Img_06 from './assets/Img_06.png';
 
 function App() {
+
+  const { ref:Roadanimation , playState, getAnimation } = useWebAnimations({
+    keyframes:[
+      {transform:'translate(-2950px)'}
+    ],
+    timing:{
+      duration:6000,
+      iterations : Infinity,
+    }
+  })
+  const { ref:car } = useWebAnimations({
+    keyframes:[
+      {transform:'translateY(-2px)'},
+      {transform:'translateY(3px)'},
+      {transform:'translateY(-1px)'}
+    ],
+    timing:{
+      duration:1000,
+      iterations : Infinity,
+    }
+  })
+  // ///
+  const speedUp = () => {
+    const animation = getAnimation();
+    animation.updatePlaybackRate(animation.playbackRate * 1.3);
+  };
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  >
+          <div>{playState}</div>
+         <div className="bg-img">
+         <div className="road" id='Roadanimation' ref={Roadanimation}></div>
+         <div className="car" ref={car}>
+             <img src={Img_06}  alt='carimg'/>
+         </div>
+         <button onClick={ () =>{getAnimation().play()}}>Start</button>
+         <button onClick={ () =>{getAnimation().pause()}}>Break</button>
+         <button onClick={ () =>{getAnimation().reverse()}} >Reverse</button>
+         <button  defaultValue="1"  onClick={speedUp}>IncreaseSpees</button>
+     
+    </div>
     </div>
   );
 }
